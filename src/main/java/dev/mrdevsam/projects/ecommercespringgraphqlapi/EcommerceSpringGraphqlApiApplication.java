f30cca7952b04e988e7dd49d0f25655549252adb
mrdevsam <mdshoykotislam05@gmail.com>
2023-05-26 19:54:36 +0000

    initial commit

---
+++ src/main/java/dev/mrdevsam/projects/ecommercespringgraphqlapi/EcommerceSpringGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mrdevsam.projects.ecommercespringgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringGraphqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringGraphqlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrdevsam/projects/ecommercespringgraphqlapi/EcommerceSpringGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrdevsam.projects.ecommercespringgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
